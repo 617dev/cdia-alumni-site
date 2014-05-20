e80f773eaa06f89b1d191f357938e265d5231ad2
cccpdesign <arosenthal@arosenthal-mb15.local>
2014-05-20 18:07:44 -0400

    updated styling for buttons
    updated button styling to be uniform for links next to names, added
    functionality to pull in portfolio URL, fixed horizontal rule.

--- db/functions.php
+++ db/functions.php
@@ -13,9 +13,10 @@ function getProfiles() {
 				<div class="blog-post cf">
 	            <img src="<?php echo $row['picture_url'] ?>" alt="<?php echo $row['first_name'] . ' ' . $row['last_name'] ?> Photo" class="profile-pic img-thumbnail">
 	          <h4><?php echo $row['first_name'] . ' ' . $row['last_name'] ?> | 
-	            <a href="#">Portfolio</a> | <a href="<?php echo $row['linkedin_url']; ?>"><img src="img/LinkedinIcon.png" style="width:20px"/></a> | <a class="btn btn-xs btn-default" href="mailto: <?php echo $row['email']; ?>" role="button">Email 
+	            <a class="btn btn-success btn-xs" href="<?php echo $row['portfolio_url'] ?>" target="_blank">Portfolio</a> | <a href="<?php echo $row['linkedin_url']; ?>" target="_blank"><img src="img/LinkedinIcon.png" style="width:22px; border-radius: 3px;"/></a> | <a class="btn btn-xs btn-danger" href="mailto: <?php echo $row['email']; ?>" role="button">Email 
 	              <span class="glyphicon glyphicon-envelope"></span></a></h4>
 	            <p><?php echo $row['location'] ?></p>
+	            <div class="cf">
 	            	<ul class="current positions">
 			            <strong>Current Positions:</strong>
 		            	<?php if ($row['current1_company'] !== '') {
@@ -33,9 +34,10 @@ function getProfiles() {
 		            	<?php if ($row['past1_company'] !== '') {
 		            		echo '<li>' . $row['past1_title'] . ' at ' . $row['past1_company'] . '</li>';
 		            	} ?>
-	            	</ul>         
-	          </div><!-- /.blog-post -->
+	            	</ul>  
+	            </div>       
 	           <hr>
+	          </div><!-- /.blog-post -->
 	    <?php
 			}
 		}

be986205eaedd34d00d39863d9a68682ca7cf6c4
Gene Babon <massmoves@hotmail.com>
2014-12-03 20:49:07 -0500

    Gene's edit to footer
    Minor content change to links

--- inc/footer.php
+++ inc/footer.php
@@ -4,9 +4,9 @@
   <p>&copy; CDIABU Alumni Association 2014</p>
   </div>
   <div class="col-md-4">
-  <p><a href="http://beantownweb.blogspot.com/2014/03/boston-web-tech-job-market-q1-2014.html">Boston Web Tech Market</a></p>
+  <p><a href="http://beantownweb.blogspot.com/2014/10/boston-web-tech-job-market-q4-2014.html" target="_blank">Boston Web Tech Market</a></p>
   </div>
   <div class="col-md-4">
-  <p><a href="http://beantownweb.blogspot.com/2014/01/bostons-rockstar-recruiters-q1-2014.html">Boston's Rockstar Recruiters</a></p>
+  <p><a href="http://beantownweb.blogspot.com/2014/10/bostons-rockstar-recruiters-q4-2014.html" target="_blank">Boston's Rockstar Recruiters</a></p>
   </div>
 </footer>

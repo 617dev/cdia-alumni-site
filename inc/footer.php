add0b9215dec537c7d2dfc7c8316122348c65a4e
Gene Babon <massmoves@hotmail.com>
2014-12-16 20:57:16 -0500

    Minor tweak
    Removed BU from CDIABU

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="row">
   <div class="col-md-4">
-  <p>&copy; CDIABU Alumni Association 2014</p>
+  <p>&copy; CDIA Alumni Association 2014</p>
   </div>
   <div class="col-md-4">
   <p><a href="http://beantownweb.blogspot.com/2014/10/boston-web-tech-job-market-q4-2014.html" target="_blank">Boston Web Tech Market</a></p>
